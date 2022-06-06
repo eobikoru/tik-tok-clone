d33f72e43aa043b7c7e3aa6e213991e0a2b7b2e8
eobikoru <emekadaniel916@gmail.com>
2022-06-06 15:37:48 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,94 @@
-import React from 'react';
-import './App.css';
-import Video from './Video';
+import React from "react";
+import "./App.css";
+import Video from "./Video";
 
 function App() {
   return (
     <div className="app">
-       <div className="app__videos">
-       <Video/>
-       <Video/>
-       <Video/>
-       <Video/>
-       <Video/>
-     </div>
+      <h1 style={{ color: "white" }}> emeka tiktok</h1>
+      <div className="app__videos">
+        <Video
+          url="https://v16-webapp.tiktok.com/024c64743f3078396f42624cfbc376f5/629e637f/video/tos/useast2a/tos-useast2a-ve-0068c002/a32b7d8a17dd420d99e6bd60e80413d4/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2536&bt=1268&btag=80000&cs=0&ds=3&ft=eXd.6HF2Myq8ZILYiwe2N5S6yl7Gb&mime_type=video_mp4&qs=0&rc=ZDNnaTM5M2lkZzVlOzhkOkBpM3RlOzc6Zmd2ZDMzNzczM0BjNS1eLWFgXi0xYzA0Y2FhYSNpNC0ucjRfYWJgLS1kMTZzcw%3D%3D&l=202206061426540102231190201118AEDE"
+          channel="Obikoru emeka channel"
+          description="Wow this works......"
+          song="Asake"
+          likes={123}
+          message={400}
+          shares={345}
+        />
+        <Video
+          url="https://v16-webapp.tiktok.com/d6387aecaf00f9a94c254b84de13c4ae/629e624b/video/tos/useast2a/tos-useast2a-ve-0068c002/0931291544c94036a9de22fc32b1b756/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2592&bt=1296&btag=80000&cs=0&ds=3&ft=eXd.6HF2Myq8Z-YYiwe2NUDlol7Gb&mime_type=video_mp4&qs=0&rc=O2hnZzc1ZGk2ZDo7Z2U7OUBpajNxOjk6ZnN3ZDMzNzczM0BhMi1jNl5jNV4xMDJiM2M0YSNtZnAxcjQwcjVgLS1kMTZzcw%3D%3D&l=202206061423070101891952160618FB1E"
+          channel="Pasolo channel"
+          description="dry  jokes ......"
+          song="Asake"
+          likes={923}
+          message={500}
+          shares={305}
+        />
+        <Video
+          url="https://v16-webapp.tiktok.com/6dc4fc121dbe051635e8014420f260c8/629e637d/video/tos/useast2a/tos-useast2a-ve-0068c001/5ece33fb439d4371ac4705dc98727124/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1852&bt=926&btag=80000&cs=0&ds=3&ft=eXd.6HF2Myq8ZILYiwe2N5S6yl7Gb&mime_type=video_mp4&qs=0&rc=ODc0M2g6Mzw3ODRmZ2U3Z0BpanNsaDk6ZnltZDMzNzczM0AtYS41L2MzXjQxNDVhMTAwYSNgMWJkcjQwZmFgLS1kMTZzcw%3D%3D&l=202206061426540102231190201118AEDE"
+          channel="Brain jotter channel"
+          description="funny  this works......"
+          song="burnaboy"
+          likes={923}
+          message={500}
+          shares={305}
+        />
+        <Video
+          url="https://v16-webapp.tiktok.com/44e20c552f6dc9a8ce8cd0c78b6a944f/629e63c3/video/tos/useast2a/tos-useast2a-ve-0068c004/8b2c5fbd35e64d4ea9d2adb2103f6c95/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=3172&bt=1586&btag=80000&cs=0&ds=3&ft=eXd.6HF2Myq8ZILYiwe2N5S6yl7Gb&mime_type=video_mp4&qs=0&rc=NWdpaTo4aTdmaGU6ZTw2M0BpMzQzcDQ6Zjc3OzMzNzczM0BfNGMxLWMtNS4xYy81MGEzYSNxb2ZpcjRfamxgLS1kMTZzcw%3D%3D&l=202206061426540102231190201118AEDE"
+          channel="Mr funny channel"
+          description="jesu loba this works......"
+          song="Asake"
+          likes={923}
+          message={500}
+          shares={305}
+        />
+        <Video
+          url="https://v16-webapp.tiktok.com/be99d607ef7c1ba6bf712cdda9d1b4db/629e6382/video/tos/alisg/tos-alisg-pve-0037c001/420238be85fa4c8284e3692dd9554889/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2108&bt=1054&btag=80000&cs=0&ds=3&ft=eXd.6HF2Myq8ZILYiwe2N5S6yl7Gb&mime_type=video_mp4&qs=0&rc=OGdoNTs0M2c5Zjo1NGg5NUBpM2hwbTg6ZnU0ZDMzODczNEA0M2NhLzAtXy4xNi4uYDAwYSMwa2trcjRnbC1gLS1kMS1zcw%3D%3D&l=202206061426540102231190201118AEDE"
+          channel="shank channel"
+          description="opor o......"
+          song="wizkid"
+          likes={9443}
+          message={700}
+          shares={399}
+        />
+        <Video
+          url="https://v16-webapp.tiktok.com/15828aeab05bb124b4d74f6dd135841a/629e6318/video/tos/useast2a/tos-useast2a-pve-0068/2f0e3640cbf24d0d8f2698a6c8f8e811/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=3898&bt=1949&btag=80000&cs=0&ds=3&ft=eXd.6HF2Myq8ZILYiwe2N5S6yl7Gb&mime_type=video_mp4&qs=0&rc=Zmg3OmVlO2dlaGc1NTkzM0BpajptOmc6ZjZ4PDMzNzczM0AyMWMyMzAvXl4xXmJhMTEtYSNoa3EycjRvLWhgLS1kMTZzcw%3D%3D&l=202206061426540102231190201118AEDE"
+          channel="Pasolo channel"
+          description="jesu loba this works......"
+          song="Asake"
+          likes={923}
+          message={500}
+          shares={305}
+        />
+        <Video
+          url="https://v16-webapp.tiktok.com/fcf9236f256417b9bea9f2a14a0f766c/629e631c/video/tos/useast2a/tos-useast2a-pve-0068/5f7244d918264c06adf823aa1f4ad55c/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2298&bt=1149&btag=80000&cs=0&ds=3&ft=eXd.6HF2Myq8ZILYiwe2N5S6yl7Gb&mime_type=video_mp4&qs=0&rc=OzZnZ2Y4OTxnMzo1NThoOkBpanMzOjc6ZjxpOzMzNzczM0A0YmAzMmJfNTYxNDE2NTAuYSNnanI1cjRnZm9gLS1kMTZzcw%3D%3D&l=202206061426540102231190201118AEDE"
+          channel="Pasolo channel"
+          description="jesu loba this works......"
+          song="Asake"
+          likes={923}
+          message={500}
+          shares={305}
+        />
+        <Video
+          url="https://v16-webapp.tiktok.com/cabcb01fa9e7194aa2eb197be652a27c/629e6381/video/tos/useast2a/tos-useast2a-ve-0068c004/1c0e017df2764fe894afd7d67c70b51b/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2564&bt=1282&btag=80000&cs=0&ds=3&ft=eXd.6HF2Myq8ZILYiwe2N5S6yl7Gb&mime_type=video_mp4&qs=0&rc=OjdkaWc0OzhkZDU3ZzZlPEBpamd4bGg6Zjo7OzMzNzczM0AyNDI0YGIvNjAxLjM0LS1fYSMzbWE1cjRfL25gLS1kMTZzcw%3D%3D&l=202206061426540102231190201118AEDE"
+          channel="Pasolo channel"
+          description="jesu loba this works......"
+          song="Asake"
+          likes={923}
+          message={500}
+          shares={305}
+        />
+        <Video
+          url="https://v16-webapp.tiktok.com/40f45a162b76675d2e273b9b04cf6f69/629e6382/video/tos/useast2a/tos-useast2a-ve-0068c003/c2c7abee96ad4ec2bda1e8f05f70c97e/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2686&bt=1343&btag=80000&cs=0&ds=3&ft=eXd.6HF2Myq8ZILYiwe2N5S6yl7Gb&mime_type=video_mp4&qs=0&rc=OmQ7NzNkZDtmZWQ7ZzU1M0BpajRrdWQ6ZnVzPDMzNzczM0BeMWJeMzQ0XjAxNl8xMC1eYSMzajY2cjQwbWtgLS1kMTZzcw%3D%3D&l=202206061426540102231190201118AEDE"
+          channel="Pasolo channel"
+          description="jesu loba this works......"
+          song="Asake"
+          likes={923}
+          message={500}
+          shares={305}
+        />
+      </div>
     </div>
   );
 }
